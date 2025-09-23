9f02bf864ab5c7076d9582533b42c4b36877bead
unknown <jaym991776@gmail.com>
2025-09-23 19:44:28 -0400

    Folder Structure

---
+++ apps/dashboard/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@secure-task-mgmt/dashboard"/>
+    </div>
+  );
+}
+
+export default App;
+
+
